2d9535c8b4972bc83e5a94154ee08b22150352cc
CongDon1207 <22133013@student.hcmute.edu.vn>
2025-06-15 10:08:01 +0700

    fix connection error successfully

--- docker/images/kafka/create-topic.sh
+++ docker/images/kafka/create-topic.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "[Bitnami Kafka] Waiting for broker to be ready..."
-while ! nc -z localhost 9092; do
+while ! nc -z kafka 9092; do
   sleep 1
 done
 
 echo "[Bitnami Kafka] Creating topic: quick_test"
 /opt/bitnami/kafka/bin/kafka-topics.sh \
-  --bootstrap-server localhost:9092 \
+  --bootstrap-server kafka:9092 \
   --create \
   --if-not-exists \
   --replication-factor 1 \
@@ -16,3 +16,4 @@ echo "[Bitnami Kafka] Creating topic: quick_test"
   --topic quick_test
 
 echo "[Bitnami Kafka] Topic created."
+
