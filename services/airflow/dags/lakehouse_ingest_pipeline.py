d6af97cfef4f5b7dcbb864f57d0269405b2843f4
CongDon1207 <22133013@student.hcmute.edu.vn>
2025-06-17 22:02:02 +0700

    error in airflow UI

---
+++ services/airflow/dags/lakehouse_ingest_pipeline.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+with DAG(
+    dag_id='lakehouse_ingest_pipeline',
+    default_args=default_args,
+    description='Nạp dữ liệu CSV vào Kafka, chạy Spark streaming ghi Bronze',
+    schedule_interval=None,  # chỉ chạy khi trigger thủ công
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+) as dag:
+
+    produce_csv = BashOperator(
+        task_id='produce_csv_to_kafka',
+        bash_command=(
+            "python /opt/airflow/tools/produce_csv_to_kafka.py "
+            "--bootstrap-server localhost:9094 "
+            "--topic footware_sales "
+            "--file /opt/airflow/data/FootWare_Sales_Dataset/FootWare_Wholesale_Sales_Dataset.csv"
+        )
+    )
+
+    spark_streaming = BashOperator(
+        task_id='spark_streaming_bronze',
+        bash_command=(
+            "spark-submit "
+            "--master spark://spark:7077 "
+            "/opt/airflow/spark_jobs/streaming_example.py"
+        )
+    )
+
+    produce_csv >> spark_streaming
